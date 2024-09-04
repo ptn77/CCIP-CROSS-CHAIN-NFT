cca84a87968c52f164e8ddd5926dce9144cbb789
ptn77 <ctn77@protonmail.com>
2024-09-04 15:06:39 -0600

    add abi.encodePacked to tokenURI

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract BasicNft is ERC721 {
     error BasicNft__TokenUriNotFound();
@@ -23,7 +24,29 @@ contract BasicNft is ERC721 {
         if (ownerOf(tokenId) == address(0)) {
             revert BasicNft__TokenUriNotFound();
         }
-        return s_tokenIdToUri[tokenId];
+
+        //return s_tokenIdToUri[tokenId];
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(),
+                            '", "description": "',
+                            "Phuong Nguyen's NFT",
+                            '", ',
+                            '"attributes": [{"trait_type": "',
+                            "Special Image",
+                            '", "value": 100}], "image":"',
+                            s_tokenIdToUri[tokenId],
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 
     function getTokenCounter() public view returns (uint256) {
